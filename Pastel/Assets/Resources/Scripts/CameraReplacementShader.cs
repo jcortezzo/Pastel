c7050e0d1f65270d98214c97d94866f5b40c359c
Nguyen Duong <ducnguyenduchao@gmail.com>
2020-07-26 00:55:18 -0700

    oil painting post process

---
+++ Pastel/Assets/Resources/Scripts/CameraReplacementShader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraReplacementShader : MonoBehaviour
+{
+    public Shader replacementShader;
+    public Material material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if(replacementShader != null) Camera.main.SetReplacementShader(replacementShader, "RenderType");
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, material);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
